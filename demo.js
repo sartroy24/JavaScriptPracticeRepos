c48e01c8c1ef9e7fb2f09af244c91dcf179e2674
sartroy24 <sarthakroy10@gmail.com>
2020-04-29 22:35:42 +0530

    Added switch

--- demo.js
+++ demo.js
@@ -1,1 +1,42 @@
-document.write("<h1>Hello GIt File </h1>");
+//document.write("<h1>Hello Git File </h1>");
+/*var a = 3; // '=' right-to-left
+var b = 3;
+var result = a+b; //+ left-to-right
+ document.write("The result is :"+result);
+
+*/
+
+/*var x = 7;
+if(x%2==0){
+    document.write("Even Number")
+}
+else{
+    document.write("Odd Number");
+}
+*/
+var day = 10;
+switch(day){
+    case 1:
+        document.write("<h2>Sunday</h2>");
+        break;
+    case 2:
+        document.write("<h2>Monday</h2>");
+        break;
+    case 3:
+        document.write("<h2>Tuesday</h2>");
+        break;
+    case 4:
+        document.write("<h2>Wednesday</h2>");
+        break;
+    case 5:
+        document.write("<h2>Thursday</h2>");
+        break;
+    case 6:
+        document.write("<h2>Friday</h2>");
+        break; 
+    case 7:
+        document.write("<h2>Saturday</h2>");
+        break;
+    default:
+        document.write("<h2>Wrong Input</h2>");    
+}
